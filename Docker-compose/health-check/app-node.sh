bd6b49e4269f6c3af98daf502fef1db218414173
ihab abadi <utopios@iMac-de-ihab.home>
2021-06-08 15:32:56 +0200

    correction dockerfile

--- Docker-compose/health-check/app-node.sh
+++ Docker-compose/health-check/app-node.sh
@@ -1,7 +1,6 @@
-#!bin/sh
-
-curl --fail http://localhost:3000 || exit 1
+#!bin/ash
+#curl --fail http://localhost:3000 || exit 1
 # exit 1 => unhealty 
-# exit 0 => healty
+exit 0
 
 #pour une base de donnée, on peut directement tester le healthcheck avec un select 
